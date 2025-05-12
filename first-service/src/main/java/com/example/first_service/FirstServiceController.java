b6d7b4d1bf3594de66d365fccd8084df36004aab
eoghks <zhfldk7316@naver.com>
2025-05-12 17:39:56 +0900

    1. check uri에 port 출력 기능 추가

--- first-service/src/main/java/com/example/first_service/FirstServiceController.java
+++ first-service/src/main/java/com/example/first_service/FirstServiceController.java
@@ -1,6 +1,8 @@
 package com.example.first_service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+    Environment env;
+
+    public FirstServiceController(Environment env) {
+        this.env =env;
+    }
+
     @GetMapping("/welcome")
     public String welcom(){
         return "Welcom to the First service.";
@@ -22,7 +30,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Check First Service";
+    public String check(HttpServletRequest request) {
+        log.info("Server pot={}", request.getServerPort());
+        return String.format("Check First Service on PORT %s", env.getProperty("local.server.port"));
     }
 }
