33102578635f47072106c32d75b751e2cf49f706
eoghks <zhfldk7316@naver.com>
2025-06-03 19:58:04 +0900

    1. User Service  1) JWT Token 생성 로직 추가

--- userService/src/main/java/com/example/userService/config/AuthenticationFilter.java
+++ userService/src/main/java/com/example/userService/config/AuthenticationFilter.java
@@ -4,6 +4,8 @@ import com.example.userService.dto.UserDto;
 import com.example.userService.service.UserService;
 import com.example.userService.vo.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private UserService userService;
@@ -49,5 +52,13 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             Authentication authResult) throws IOException, ServletException {
         String userName = ((User)authResult.getPrincipal()).getUsername();
         UserDto userDetails = userService.getUserDetailsByEmail(userName);
+
+        String token = Jwts.builder()
+                .setSubject(userDetails.getUserId())
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(env.getProperty("token.expiration_time"))))
+                .signWith(SignatureAlgorithm.HS512, env.getProperty("token.secret"))
+                .compact();
+        response.addHeader("token", token);
+        response.addHeader("userId", userDetails.getUserId());
     }
 }
